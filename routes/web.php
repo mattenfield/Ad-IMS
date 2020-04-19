ce8108fb3d241ec8c60f72db9d42f8235aba6781
Matt Enfield <44966386+mattenfield@users.noreply.github.com>
2020-04-19 17:28:24 +0100

    Removed check for image config

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,6 @@
 */
 
 Auth::routes(['register' => false]);
-Route::get('/file-config',  function() {
-    return config('filesystems');
-});
 
 Route::group(['middleware' => 'admin'], function () {
     Route::get('/manageusers/search', 'ManageUsersController@search')->name('managesearch');
