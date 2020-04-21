3c6937293932510fc949be6a21ac4592ce425d5d
Matt Enfield <44966386+mattenfield@users.noreply.github.com>
2020-04-21 14:51:35 +0100

    #7 - changed evidence to photo

--- resources/views/stockadd.blade.php
+++ resources/views/stockadd.blade.php
@@ -54,7 +54,7 @@
                             
                         </div>
                         <div class="form-group row">
-                            <label for="select_file" class="col-md-4 col-form-label text-md-right">{{ __('Upload Evidence') }}</label>
+                            <label for="select_file" class="col-md-4 col-form-label text-md-right">{{ __('Upload Photo') }}</label>
 
                             
                             <input  type="file" name = "select_file">
